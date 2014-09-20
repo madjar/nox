6dd1610111f93faeee642aee2c47f2fc61de5045
Georges Dubus <georges.dubus@compiletoi.net>
2014-09-20 22:31:41 +0200

    Made default.nix override the nox defined in nixpkgs

--- default.nix
+++ default.nix
@@ -1,29 +1,16 @@
 with import <nixpkgs> { };
+let characteristic = python3Packages.buildPythonPackage rec {
+  name = "characteristic-14.1.0";
+  src = fetchurl {
+    url = "https://pypi.python.org/packages/source/c/characteristic/characteristic-14.1.0.tar.gz";
+    md5 = "68ea7e28997fc57d3631791ec0567a05";
+  };
+  doCheck = false;
+};
+  newDeps = [ python3Packages.requests2 characteristic ];
 
-
-python34Packages.buildPythonPackage {
-  name = "nox-HEAD";
+in stdenv.lib.overrideDerivation nox (oldAttrs : {
   src = ./.;
-  buildInputs = [ git python34Packages.pbr makeWrapper ];
-  pythonPath =
-    [ python34Packages.dogpile_cache
-      python34Packages.click
-      python34Packages.requests2
-      (python34Packages.buildPythonPackage rec {
-        name = "characteristic-14.1.0";
-
-        src = fetchurl {
-          url = "https://pypi.python.org/packages/source/c/characteristic/characteristic-14.1.0.tar.gz";
-          md5 = "68ea7e28997fc57d3631791ec0567a05";
-        };
-        doCheck = false;
-      })
-    ];
-  postInstall = "wrapProgram $out/bin/nox --prefix PATH : ${nixUnstable}/bin";
-}   
-
-# TODO : make this override work
-# nox.override (oldAttrs : {
-#   src = ./.;
-#   pythonPath = oldAttrs.pythonPath ++ [ pythonPackages.requests2 ];
-# })
+  pythonPath = oldAttrs.pythonPath ++ newDeps;
+  nativeBuildInputs = oldAttrs.nativeBuildInputs ++ newDeps;
+})
