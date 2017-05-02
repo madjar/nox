bf6d5c637f0f50494982d23199e3726397d778ed
Linus Heckemann <git@sphalerite.org>
2017-05-02 14:30:11 +0100

    Remove interactive prompt

--- nox/search.py
+++ nox/search.py
@@ -72,22 +72,3 @@ def main(query, force_refresh):
             click.style(p.attribute, dim=True),
             click.style(p.description.replace("\n", "\n    ")))
         click.echo(line)
-
-    if results:
-        def parse_input(inp):
-            if inp[0] == 's':
-                action = 'shell'
-                inp = inp[1:]
-            else:
-                action = 'install'
-            packages = [results[int(i) - 1] for i in inp.split()]
-            return action, packages
-
-        action, packages = click.prompt('Packages to install',
-                                        value_proc=parse_input)
-        attributes = [p.attribute for p in packages]
-        if action == 'install':
-            subprocess.check_call(['nix-env', '-iA', '--show-trace'] + attributes)
-        elif action == 'shell':
-            attributes = [a[len('nixpkgs.'):] for a in attributes]
-            subprocess.check_call(['nix-shell', '-p', '--show-trace'] + attributes)
